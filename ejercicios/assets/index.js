6f93b06f9c55e952d298f8a7d2afccc9948b9bcf
Duartte <el@duartte.co>
2021-03-16 17:06:01 +0100

    this

--- ejercicios/assets/index.js
+++ ejercicios/assets/index.js
@@ -1,4 +1,4 @@
-import MediaPlayer from "./MediaPlayer.js";
+import MediaPlayer from "../../assets/MediaPlayer.js";
 import AutoPlay from "./plugins/AutoPlay.js";
 
 const video = document.querySelector("video");
